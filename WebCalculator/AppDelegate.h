43e55c2ecbc42f823294d517ed361e5ebaeed6fd
Gabriel_Azevedo <gabriel.azevedo95@icloud.com>
2015-01-09 09:14:54 -0800

    Initial Commit

---
+++ WebCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebCalculator
+//
+//  Created by Gabriel Borri de Azevedo on 1/9/15.
+//  Copyright (c) 2015 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
